6e7de6bca123ee14b0e7183318d5401fa0950dc3
NavuruSahithya <sahithyanavuru@gmail.com>
2024-08-29 12:09:34 +0530

    Sajxvb

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const alienRouter = require('./routes/aliens')
 
 
 //const url = 'mongodb://127.0.0.1:27017/CBIT'
-const url='mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101'
+//const url='mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101'
+const url='mongodb+srv://sahithyanavuru:7670899051@merncrud.dbane.mongodb.net/?retryWrites=true&w=majority&appName=MERNCRUD'
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
@@ -19,7 +20,7 @@ console.log('connected...')
 app.use(express.json())
 
 app.use('/aliens',alienRouter)
-app.listen(9000, () =>
+app.listen(9008, () =>
 {
 console.log('Server started')
 })
